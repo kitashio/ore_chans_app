608438867a6c31258483459c19762738b6570a93
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-09-13 16:12:16 +0900

    delete: 不要なフォルダを削除

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ore_chans_app/doc_firebase/src/features/auth/data/auth_provider.dart';
 import 'package:ore_chans_app/doc_firebase/src/features/auth/presentation/signin_page.dart';
-import 'package:ore_chans_app/doc_firebase/src/features/firestore_crud/presentation/post_page.dart';
+import 'package:ore_chans_app/doc_firebase/src/features/post_crud_app/presentation/view/post_page.dart';
 import 'package:ore_chans_app/firebase_options.dart';
 
 Future<void> main() async {
