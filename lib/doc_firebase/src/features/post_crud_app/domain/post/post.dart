fb00e90afd3da92a18e9c92ca8f200667ed19017
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-09-14 12:02:39 +0900

    remove: コメントを削除

--- lib/doc_firebase/src/features/post_crud_app/domain/post/post.dart
+++ lib/doc_firebase/src/features/post_crud_app/domain/post/post.dart
@@ -5,18 +5,8 @@ import 'package:ore_chans_app/doc_firebase/src/features/post_crud_app/domain/con
 part 'post.freezed.dart';
 part 'post.g.dart';
 
-/// [コマンドを実行したら起動したままにするwatchがついたコマンド]
-/// watchがあると、毎回コマンドを実行しなくて良くなる。
 // flutter pub run build_runner watch --delete-conflicting-outputs
 
-/// [postコレクションのドキュメントのモデルクラス]
-/*Freezedを使うと何がうれしいのか?
-toJson()やfromJson()を自動生成してくれるので、
-モデルクラスを作るときに、
-toJson()やfromJson()を書く手間が省ける。
-copyWith()も自動生成してくれるので、
-コンストラクターの引数を省略したいときに便利。
- */
 @freezed
 class Post with _$Post {
   const factory Post({
