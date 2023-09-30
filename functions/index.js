f3ddcd2afc496aeedb14e1fc24f225056f5e3803
onishi taisei <taisei.developer@gmail.com>
2023-09-30 15:12:10 +0900

    Add region to fetchData function for deployment

--- functions/index.js
+++ functions/index.js
@@ -2,35 +2,37 @@ const functions = require("firebase-functions");
 const https = require("https");
 
 // APIサンプル
-exports.fetchData = functions.https.onRequest((_, response) => {
-    const options = {
-        hostname: "jsonplaceholder.typicode.com",
-        path: "/posts/1",
-        method: "GET",
-    };
+exports.fetchData = functions
+    .region("asia-northeast1")
+    .https.onRequest((_, response) => {
+        const options = {
+            hostname: "jsonplaceholder.typicode.com",
+            path: "/posts/1",
+            method: "GET",
+        };
 
-    const req = https.request(options, (res) => {
-        let data = "";
+        const req = https.request(options, (res) => {
+            let data = "";
 
-        res.on("data", (chunk) => {
-            data += chunk;
+            res.on("data", (chunk) => {
+                data += chunk;
+            });
+
+            res.on("end", () => {
+                try {
+                    const parsedData = JSON.parse(data);
+                    response.send(parsedData);
+                } catch (e) {
+                    console.error("Error parsing JSON:", e);
+                    response.status(500).send("Server Error");
+                }
+            });
         });
 
-        res.on("end", () => {
-            try {
-                const parsedData = JSON.parse(data);
-                response.send(parsedData);
-            } catch (e) {
-                console.error("Error parsing JSON:", e);
-                response.status(500).send("Server Error");
-            }
+        req.on("error", (e) => {
+            console.error(`Problem with request: ${e.message}`);
+            response.status(500).send("Server Error");
         });
-    });
 
-    req.on("error", (e) => {
-        console.error(`Problem with request: ${e.message}`);
-        response.status(500).send("Server Error");
+        req.end();
     });
-
-    req.end();
-});
