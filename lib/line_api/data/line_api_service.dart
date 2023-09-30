aa696d3b7cf2239682561a8e754ae7a5bef26d35
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-09-30 22:30:00 +0900

    delete: file

--- lib/line_api/data/line_api_service.dart
+++ lib/line_api/data/line_api_service.dart
@@ -1,18 +1,18 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ore_chans_app/line_api/domain/line_message.dart';
-import 'package:retrofit/retrofit.dart';
-import 'package:dio/dio.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:ore_chans_app/line_api/domain/line_message.dart';
+// import 'package:retrofit/retrofit.dart';
+// import 'package:dio/dio.dart';
 
-part 'line_api_service.g.dart';
+// part 'line_api_service.g.dart';
 
-final lineApiServiceProvider = Provider((ref) => LineApiService(Dio()));
+// final lineApiServiceProvider = Provider((ref) => LineApiService(Dio()));
 
-// LINE Messaging APIのサービスを定義する
-@RestApi(baseUrl: "https://api.line.me/v2/bot/")
-abstract class LineApiService {
-  factory LineApiService(Dio dio, {String baseUrl}) = _LineApiService;
-  // LINEグループにメッセージを送信するメソッド
-  @POST("message/push")
-  Future<void> pushMessage(
-      @Header("Authorization") String token, @Body() LinePushMessage message);
-}
+// // LINE Messaging APIのサービスを定義する
+// @RestApi(baseUrl: "https://api.line.me/v2/bot/")
+// abstract class LineApiService {
+//   factory LineApiService(Dio dio, {String baseUrl}) = _LineApiService;
+//   // LINEグループにメッセージを送信するメソッド
+//   @POST("message/push")
+//   Future<void> pushMessage(
+//       @Header("Authorization") String token, @Body() LinePushMessage message);
+// }
