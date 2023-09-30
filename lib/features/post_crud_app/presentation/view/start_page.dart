e12f9ffb6fde2f134dee90d6ea7ccd6834ee0d97
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-10-01 08:20:51 +0900

    refactor: constractor value

--- lib/features/post_crud_app/presentation/view/start_page.dart
+++ lib/features/post_crud_app/presentation/view/start_page.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:ore_chans_app/utils/main_button_component.dart';
 import 'package:ore_chans_app/utils/name_generator.dart';
 
 /// テスト開始画面
 class StartPage extends ConsumerWidget {
-  const StartPage({
-    super.key,
+  StartPage({
+    Key? key,
     required this.imagePath,
-  });
+  })  : name = AnimeCharacterNameGenerator().getRandomName(),
+        tts = FlutterTts(),
+        super(key: key) {
+    tts.setLanguage('ja-JP');
+    tts.setSpeechRate(0.2);
+  }
 
   final String imagePath;
+  final String name;
+  final FlutterTts tts;
+
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -40,7 +49,7 @@ class StartPage extends ConsumerWidget {
               mainAxisSize: MainAxisSize.min,
               children: [
                 Text(
-                  "${AnimeCharacterNameGenerator().getRandomName()}ちゃん",
+                  "${name}ちゃん",
                   style: const TextStyle(
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
@@ -49,10 +58,15 @@ class StartPage extends ConsumerWidget {
                   textAlign: TextAlign.center,
                 ),
                 const SizedBox(width: 12),
-                const Icon(
-                  Icons.volume_up,
-                  color: Colors.white,
-                  size: 32,
+                IconButton(
+                  onPressed: () {
+                    tts.speak(name);
+                  },
+                  icon: const Icon(
+                    Icons.volume_up,
+                    color: Colors.white,
+                    size: 32,
+                  ),
                 ),
               ],
             ),
