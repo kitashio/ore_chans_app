88ae4d7b95fa5e4da3142a06739857b651b2fca6
kitashio <kitashio.pr@gmail.com>
2023-10-01 03:24:07 +0900

    fix: :bug: import削除

--- lib/features/auth/presentation/signin_page.dart
+++ lib/features/auth/presentation/signin_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ore_chans_app/extension/async_value_extension.dart';
-import 'package:ore_chans_app/features/auth/application/auth_notifier.dart';
 import 'package:ore_chans_app/features/image_generator/application/generate_image.dart';
 import 'package:ore_chans_app/utils/main_button_component.dart';
 
